b0e4762121072d40602186dd4cca44d82021ff51
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-07-26 23:16:52 +0700

    fix maps

--- index.php
+++ index.php
@@ -82,77 +82,72 @@ require 'system/config/koneksi.php'
       </div>
     </div>
   </div>
-
   </div>
 
-
   <!--konten2-->
+  <div class="space">
+    <br>
+    <br>
+  </div>
   <div id="foo">
-    <section class="team">
-      <div class="container">
-        <div class="row">
-          <h1>TERTARIK BERGABUNG ???</h1>
-          <p>BERAMAL (Bener Ramah Lingkungan) adalah sebuah wadah masyarakat dalam pengelolaan sampah di Kampung Bener
-            Yogyakarta. Salah satu program dari BERAMAL adalalah Bank Sampah. Bank sampah berpotensi menurunkan hingga
-            setengah dari produksi sampah anorganik. Selain lingkungan rumah menjadi bersih dari sampah anorganik, juga
-            akan lebih sehat karena mengurangi perkembangbiakan penyakit seperti Demam Berdarah, mengurangi polusi udara
-            akibat pembakaran sampah, dan tanah menjadi lebih subur karena tidak tercemar oleh sampah yang tidak dapat
-            terurai. Si sisi lain, bank sampah dapat menambah penghasilan bagi para nasabahnya.</p>
-        </div>
-
-        <div class="row mgt50px">
-          <div class="coloumn">
-            <div class="imgBox">
-              <img src="asset/internal/img/img-content/1.jpg">
-            </div>
-            <div class="details">
-              <h3>#Tahap1<br><span>Lakukan Pendaftaran</span></h3>
-            </div>
-          </div>
-          <div class="coloumn">
-            <div class="imgBox">
-              <img src="asset/internal/img/img-content/2.jpg">
-            </div>
-            <div class="details">
-              <h3>#Tahap2<br><span>Pemilahan Sampah</span></h3>
-            </div>
-          </div>
-          <div class="coloumn">
-            <div class="imgBox">
-              <img src="asset/internal/img/img-content/3.jpg">
-            </div>
-            <div class="details">
-              <h3>#Tahap3<br><span>Penimbangan Sampah</span></h3>
-            </div>
-          </div>
-          <div class="coloumn">
-            <div class="imgBox">
-              <img src="asset/internal/img/img-content/4.jpg">
-            </div>
-            <div class="details">
-              <h3>#Tahap4<br><span>Mendapat Keuntungan</span></h3>
-            </div>
-          </div>
-          <div style="clear: both;"></div>
-        </div>
+    <div class="container">
+      <div class="row">
+        <h1>Petunjuk</h1>
+        <img src="asset/internal/img/img-local/petunjuk.png" alt="">
       </div>
+
+      <div class="row mgt50px">
+        <div class="coloumn">
+          <div class="imgBox">
+            <img src="asset/internal/img/img-content/1.jpg">
+          </div>
+          <div class="details">
+            <h3><span>Lakukan Pendaftaran</span></h3>
+          </div>
+        </div>
+        <div class="coloumn">
+          <div class="imgBox">
+            <img src="asset/internal/img/img-content/2.jpg">
+          </div>
+          <div class="details">
+            <h3><span>Pemilahan Sampah</span></h3>
+          </div>
+        </div>
+        <div class="coloumn">
+          <div class="imgBox">
+            <img src="asset/internal/img/img-content/3.jpg">
+          </div>
+          <div class="details">
+            <h3><span>Penimbangan Sampah</span></h3>
+          </div>
+        </div>
+        <div class="coloumn">
+          <div class="imgBox">
+            <img src="asset/internal/img/img-content/4.jpg">
+          </div>
+          <div class="details">
+            <h3><span>Dapatkan Keuntungan</span></h3>
+          </div>
+        </div>
+        <div style="clear: both;"></div>
+      </div>
+    </div>
     </section>
   </div>
 
   <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
   <script src="asset/internal/js/index.js"></script>
 
-  <!--konten maps-->
-
+  <!--Konten Maps-->
   <br>
   <div id="bar">
     <div class="row mgt50px">
-      <h1 align="center">Lokasi Bank Sampah</h1>
-      <br>
+      <h1>Lokasi Bank Sampah</h1>
       <br>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7906.271832592935!2d110.34985657422243!3d-7.775409477585481!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a586aaf61cbf1%3A0xed59773d37f91c28!2sBener%2C%20Tegalrejo%2C%20Yogyakarta%20City%2C%20Special%20Region%20of%20Yogyakarta!5e0!3m2!1sen!2sid!4v1644446298849!5m2!1sen!2sid"
-        width="1200" height="600" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        width="1500" height="600" style="border:0;" allowfullscreen="" loading="lazy">
+      </iframe>
     </div>
   </div>
 
@@ -160,16 +155,13 @@ require 'system/config/koneksi.php'
   <footer class="footer-distributed">
 
     <div class="footer-left">
-
       <p class="footer-links">
         <ul>
           <a href="#">Beranda</a>
-          ·
           <a href="#foo">Petunjuk</a>
-          ·
           <a href="#baz">Lokasi</a>
       </p>
-      <p class="footer-company-name">&copy; BERAMAL2022</p>
+      <p class="footer-company-name">&copy;BERAMAL2022</p>
     </div>
 
     <div class="footer-center">
