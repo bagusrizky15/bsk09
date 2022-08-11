ca9efa64654661faa290363b8ae2d620874425ef
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2022-08-11 16:18:09 +0700

    add print

--- index.php
+++ index.php
@@ -6,8 +6,10 @@ require 'system/config/koneksi.php'
 <html>
 
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset="UTF-8">
   <title>Beramal</title>
+  
   <link href="https://fonts.googleapis.com/css?family=Montserrat|Contrail+One|Raleway" rel="stylesheet">
   <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
